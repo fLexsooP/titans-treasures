466c0b364f6678a49c401679d2b46b4a608cb7f6
Brian Yang <brianyang@csu.fullerton.edu>
2023-03-08 17:29:47 -0800

    modified hello programs

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+
+class Hello {
+    public:
+        void say_hello(std::string to) {
+            std::cout << "Hello, " << to << "!" << std::endl;
+        }
+};
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    Hello hello_obj = Hello();
+    hello_obj.say_hello("World");
+    hello_obj.say_hello("Main");
+
     return 0;
 }
