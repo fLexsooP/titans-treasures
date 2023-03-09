0e446f2a593843b98e0c6a363b17d1b88049afdd
Brian Yang <brianyang@csu.fullerton.edu>
2023-03-08 17:29:09 -0800

    Revert "modified hello programs"
    This reverts commit 7090f2e6db0dc47072baad345c1c2d8e0f99e57e.

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
-#include <string>
-
-class Hello {
-    public:
-        void say_hello(std::string to) {
-            std::cout << "Hello, " << to << "!" << std::endl;
-        }
-};
 
 int main() {
-    Hello hello_obj = Hello();
-    hello_obj.say_hello("World");
-    hello_obj.say_hello("Main");
-
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
--- hello-world.py
+++ hello-world.py
@@ -1,9 +1,1 @@
-class Hello:
-    def say_hello(self, to: str):
-        print(f"Hello, {to}!")
-
-hello_obj = Hello()
-hello_obj.say_hello("World")
-
-if (__name__ == "__main__"):
-    hello_obj.say_hello("Main")
+print("Hello World")
