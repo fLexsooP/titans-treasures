d0927ad2c9b3f4d5170f4865fe4da281762ba3c1
Chence Shi <ascc594557067@gmail.com>
2023-04-10 02:15:14 -0700

    add date restriction for bid

---
+++ Restrictions/bid.js
@@ -0,0 +1,9 @@
+const expDate = '2023-04-12';
+
+const currentDate = new Date();
+
+const closingDate = new Date(closingDate);
+
+if (currentDate > closingDate) {
+  document.getElementById('bidButton').disabled = true;
+}
