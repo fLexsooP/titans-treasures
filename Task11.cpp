2009e03f1d5d93311293a5a7bf92e76d6b39fa59
JoeyNTS <joeysandoval@csu.fullerton.edu>
2023-04-19 17:44:53 -0700

    done

---
+++ Task11.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Update the buyer UI view to display the searched products from highest review scores to lowest review scores & vice versa when filtering searches by review scores" << std::endl;
+    return 0;
+}
---
+++ Task12.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Update the buyer UI view to display the searched products from highest prices to lowest prices & vice versa when filtering searches by price" << std::endl;
+    return 0;
+}
