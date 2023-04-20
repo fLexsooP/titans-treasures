60e00034e961344cc3666fe5255d68bfd7d29d97
Brian Yang <brianyang@csu.fullerton.edu>
2023-04-19 17:45:39 -0700

    updated buyer view (T8, T9)

--- views/Buyer.js
+++ views/Buyer.js
@@ -1,1 +1,5 @@
 console.log('This is the Buyer view');
+
+// 4.19.23 updates
+console.log("Display the searched products from closest to farthest distance & vice versa when using filtering searches by distance")
+console.log("Display the searched products in alphabetical order & reverse alphabetical order & vice versa when filtering searches by vendors")
