fb67f3b35297b611b1b534cd02ea2d0a768910ca
Chence Shi <ascc594557067@gmail.com>
2023-04-19 17:01:34 -0700

    add task 4,5,6 for sprint 2

---
+++ sprint2-task456.py
@@ -0,0 +1,3 @@
+print("""Implement the option to fiilter a product by upload dates
+Implement the option to filter by best selling products
+Implement the option to search for products based on their price""")
