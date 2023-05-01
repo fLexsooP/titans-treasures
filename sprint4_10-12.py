2db477951ce3835d2f23b2045ee0be06219fc87b
Chence Shi <83258121+fLexsooP@users.noreply.github.com>
2023-04-30 23:25:24 -0700

    complete sprint4_10-12.py

---
+++ sprint4_10-12.py
@@ -0,0 +1,5 @@
+print('''
+Implement a report button
+Implement the ability to send a friend request to another user 
+Implement the ability to reject and accept a friend request and update database to reflect the change
+''')
