826361b0bd0ca400d7c1fc800e7d0fd68d1e83bd
JoeyNTS <joeysandoval@csu.fullerton.edu>
2023-04-30 20:05:03 -0700

    done

---
+++ Task04.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Update the database to contain  a vector that contains all the accounts a user has blocked" << std::endl;
+    return 0;
+}
---
+++ Task05.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Update the UI with a tab that displays the message history of the user" << std::endl;
+    return 0;
+}
