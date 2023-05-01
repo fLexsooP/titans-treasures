7ee63638970727fb84306d86b230708903c482eb
mnanthav <mya.nanthavong@gmail.com>
2023-05-01 07:58:52 -0700

    Sprint4_7: Added request for user location

---
+++ userCurrentLoc.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+main() {
+
+    //task 8
+    std::cout << "Updated buyer view to display products in a variance of distances" 
+    << "Search products closest to furthest or vice versa based on user location";
+
+    //task 9
+    std::cout << "Updated buyer view to filter by vendor"
+    << "Adds ability to search products alphabetical in ascending or descending order or vice versa";
+
+
+
+
+    return 0;
+}
