86621a4f232812dbd4c779947f25d7803738b75a
mnanthav <mya.nanthavong@gmail.com>
2023-05-01 08:00:00 -0700

    Sprint4_8-9 Updated buyer UI to display products by distance/alphabetical

---
+++ updateBuyerUI.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+main() {
+
+    //task 7
+    std::cout << "Request user to share location with app" 
+    << "User can share always or only while using";
+
+    return 0;
+}
